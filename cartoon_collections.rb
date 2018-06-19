6e1ca41b3ae60aaff427e7fdc6fedc947054d000
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-19 19:08:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(planeteer_calls) # code an argument here
   planeteer_calls.each do |x|
     capitalized << x.upcase
   end
-  
+  capitalized
 end
 
 def long_planeteer_calls# code an argument here
